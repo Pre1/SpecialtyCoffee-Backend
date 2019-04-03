f0663d766bd568ace3368fc0258b3af48886f14f
abdullah <anabanmi@hotmail.com>
2019-04-03 23:22:45 +0300

    customizing admin page

--- api/admin.py
+++ api/admin.py
@@ -8,9 +8,25 @@ from .models import (
   Order,
 )
 
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['name', 'price', 'is_avaliable']
+
+class OrderProductAdmin(admin.ModelAdmin):
+	list_display = ['product', 'quantity', 'order', 'total_price']
+
+class OrderProductInline(admin.StackedInline):
+	model = OrderProduct
+	extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+	list_display = ['id', 'ordered_by', 'total_price', 'status']
+	inlines = [OrderProductInline]
+
 admin.site.register(Status)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Profile)
-admin.site.register(OrderProduct)
-admin.site.register(Order)
+admin.site.register(OrderProduct, OrderProductAdmin)
+admin.site.register(Order, OrderAdmin)
+
+
 
